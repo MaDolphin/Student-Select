af9ab4741c9bfe9ef8ea9040afaf3fc4630b7a7f
黄海琨 <huanghaikun119119@hotmail.com>
2016-10-28 13:35:44 +0800

    hhk

--- src/main/java/com/demo/controller/AdminController.java
+++ src/main/java/com/demo/controller/AdminController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static java.lang.System.out;
 
 /**
  * Created by lenovo on 2016/10/25.
@@ -26,15 +32,37 @@ public class AdminController {
 
     @RequestMapping("/batchImportMethod")
     public String batchImportMethod(MultipartFile upload1,MultipartFile upload2,MultipartFile upload3, Model model){
-        try {
-            if(upload1.getSize() != 0)
-                managerService.studentFileToDB(upload1);
-            if(upload2.getSize() != 0)
-                managerService.teacherFileToDB(upload2);
-            if(upload3.getSize() != 0)
-                managerService.majorFileToDB(upload3);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(upload1.getSize() != 0){
+            boolean flag1 = false;
+            try{
+                if(managerService.studentFileToDB(upload1))
+                    flag1 = true;
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        if(upload2.getSize() != 0){
+            boolean flag2 = false;
+            try{
+                if(managerService.teacherFileToDB(upload1))
+                    flag2 = true;
+                else flag2 = false;
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        if(upload3.getSize() != 0){
+            boolean flag3 = false;
+            try{
+                if(managerService.majorFileToDB(upload1))
+                    flag3 = true;
+                else flag3 = false;
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
         return "redirect:/admin/batchImport";
     }
--- src/main/java/com/demo/entity/Intention.java
+++ src/main/java/com/demo/entity/Intention.java
@@ -19,6 +19,16 @@ public class Intention {
     
     private Student student;
 
+    private Teacher teacher;
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
     public Student getStudent() {
         return student;
     }
